249bc659b63b7b26748efe57efddd42b7cb8f8f6
prahalad12345 <prahaladvijaykumar@gmail.com>
2025-08-11 21:46:45 +0530

    Modified client

--- Socket/Client/client.py
+++ Socket/Client/client.py
@@ -1,4 +1,5 @@
 # Import socket module 
+'''
 import socket             
 
 # Create a socket object 
@@ -14,3 +15,42 @@ s.connect(('127.0.0.1', port))
 print (s.recv(1024).decode())
 # close the connection 
 s.close()
+'''
+
+import asyncio
+import socket 
+
+
+class Client:
+    def __init__(self,ip,port):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.port = port
+        self.ip = ip
+
+    def connect(self):
+        try:
+            self.client.connect((self.ip, self.port))
+            print(f"Connected to server at {self.ip}:{self.port}")
+        except socket.error as e:
+            print(f"Connection error: {e}")
+
+    
+    def  recieve_message(self):
+        try:
+            str = self.client.recv(1024).decode()
+            print(f"Received from server: {str}")
+        except socket.error as e:
+            print(f"Error receiving message: {e}")
+    
+
+    def close(self):
+        self.client.close()
+        print("Connection closed.")    
+
+if __name__ == '__main__':
+    client = Client('127.0.0.1', 51003)
+    client.connect()
+    client.recieve_message()
+    client.close()
+    print("THE END")
+    
